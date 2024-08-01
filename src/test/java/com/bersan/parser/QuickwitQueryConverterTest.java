d76fc63a9c4fc6d7086c14ae679f665584be95af
Berşan Kılıç <bersanklc02@gmail.com>
2024-08-01 14:51:13 +0300

    Add unit test for converting Between expression to Quickwit query

---
+++ src/test/java/com/bersan/parser/QuickwitQueryConverterTest.java
@@ -0,0 +1,33 @@
+package com.bersan.parser;
+
+import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.expression.operators.relational.Between;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class QuickwitQueryConverterTest {
+    
+    @Test
+    void convertExpressionToQuickwit_shouldConvertBetweenExpression() {
+        Between between = mock(Between.class);
+        Expression leftExpression = mock(Expression.class);
+        Expression startExpression = mock(Expression.class);
+        Expression endExpression = mock(Expression.class);
+
+        when(between.getLeftExpression()).thenReturn(leftExpression);
+        when(between.getBetweenExpressionStart()).thenReturn(startExpression);
+        when(between.getBetweenExpressionEnd()).thenReturn(endExpression);
+        when(leftExpression.toString()).thenReturn("field");
+        when(startExpression.toString()).thenReturn("10");
+        when(endExpression.toString()).thenReturn("20");
+
+        QuickwitQueryConverter converter = new QuickwitQueryConverter();
+        String result = converter.convertExpressionToQuickwit(between);
+
+        assertEquals("field:[10 TO 20]", result);
+    }
+
+    
+}
